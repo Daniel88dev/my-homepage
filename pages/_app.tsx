cd67a418f02cf44c0809ecdbfc6675ed0be7aa44
Daniel Hrynusiw <64728456+Daniel88dev@users.noreply.github.com>
2024-02-04 16:17:27 +0100

    Add Latin subsets to Poppins font and adjust CSS margins
    Added "latin" to the subsets of the Poppins font in the _app.tsx file for broader language support. Also, a bottom margin was added to the hero module text style for improved layout and readability. Furthermore, TaskOptions in watcherTasks.xml is now disabled by setting isEnabled to false for optimized development environment.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ weight: ["100", "200", "400", "700", "900"] });
+const poppins = Poppins({
+  weight: ["100", "200", "400", "700", "900"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
