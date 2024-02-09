0f83c62150df95c7f0ecc8f2cf9310ab236080e9
Daniel Hrynusiw <64728456+Daniel88dev@users.noreply.github.com>
2024-02-09 17:33:31 +0100

    Update 'DTC List' project link and description in Projects component
    The project link and certain details in the description for the 'DTC List' project have been updated in the Projects component. The new project link is more succinct, and the description now includes that access to the DTC codes is password-protected.

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -66,15 +66,14 @@ const projects = [
     title: "DTC List",
     imgSrc: "project-imgs/DTC_list.png",
     code: "https://github.com/Daniel88dev/dtc-list",
-    projectLink:
-      "https://dtc-list-8krmhskl6-daniel-hrynusiws-projects.vercel.app/",
+    projectLink: "https://dtc-list.vercel.app/",
     tech: ["React", "Typescript", "Tailwind"],
     description: "Web app to search, and display DTC",
     modalContent: (
       <>
         <p>
           Added large amount of DTC codes, with Filtering based on entered DTC
-          code.
+          code. Access is protected by password.
         </p>
         <p>
           DTC means Diagnosis Trouble Code, which are codes displaying some type
