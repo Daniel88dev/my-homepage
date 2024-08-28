d31577717c7af86166e83ab67f06d01de4bef5ef
Daniel88dev <daniel.hrynusiw@gmail.com>
2024-08-28 18:49:58 +0200

    Updated hero page to calculate years spent on web development

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -5,6 +5,20 @@ import { ReactTyped } from "react-typed";
 import styles from "./hero.module.scss";
 // TODO count years of experience
 export const Hero = () => {
+  const calculateYearSpent = () => {
+    const startDate = new Date("2021-09-02");
+    const now = new Date();
+
+    const diffInMs = now.getTime() - startDate.getTime();
+
+    const msInYear = 1000 * 60 * 60 * 24 * 362.25;
+
+    const yearsSpent = diffInMs / msInYear;
+
+    return parseFloat(yearsSpent.toFixed(1));
+  };
+
+  const yearsSpent = calculateYearSpent();
   return (
     <section className={`section-wrapper ${styles.hero}`}>
       <div className={styles.copyWrapper}>
@@ -33,13 +47,14 @@ export const Hero = () => {
         </Reveal>
         <Reveal>
           <p className={styles.aboutCopy}>
-            I&apos;ve spent the last 2 years (from 2021 September) learning, and
-            developing web projects. Finished several courses for Frontend, and
-            Backend development. So Full Stack development isn&apos;t problem
-            for me. <br />I am working also as Manufacturer Engineer at Hyundai
-            Motor Manufacturing s.r.o. Where i am responsible for Equipment
-            preparation for new vehicle model production, solving Manufacturing
-            problems, and implementing those Models in to production.
+            I&apos;ve spent the last {yearsSpent} years (from 2021 September)
+            learning, and developing web projects. Finished several courses for
+            Frontend, and Backend development. So Full Stack development
+            isn&apos;t problem for me. <br />I am working also as Manufacturer
+            Engineer at Hyundai Motor Manufacturing s.r.o. Where i am
+            responsible for Equipment preparation for new vehicle model
+            production, solving Manufacturing problems, and implementing those
+            Models in to production.
             <br />I have experience with Project Management of several projects,
             to improve workability, and save money in long term production.
           </p>
