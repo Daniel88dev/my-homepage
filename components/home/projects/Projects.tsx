765d08a8c6355d645729f05b544dac30e7e94277
Daniel88dev <64728456+Daniel88dev@users.noreply.github.com>
2025-03-02 15:39:07 +0100

    Added more projects to portfolio: CFT Problem List, E-Plant Shopping, Express book review

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -63,10 +63,77 @@ const projects = [
     ),
   },
   {
+    title: "CFT Problem List",
+    imgSrc: "/project-imgs/cft_problem_list.png",
+    code: "https://github.com/Daniel88dev/cft-problem-lists",
+    projectLink: "https://cft-problem-lists.vercel.app/",
+    tech: ["React", "Javascript", "HTML", "CSS Modules"],
+    description:
+      "Project to manage CFT (Cross Functional Team) Problems on new Vehicle model Development",
+    modalContent: (
+      <>
+        <p>UNFINISHED</p>
+        <p>
+          Project was made to help managing problems during new vehicle
+          development. Later on we started to use M365 in work, and different
+          approach was decided to manage problems.
+        </p>
+        <p>It was one of first project I started to work on.</p>
+      </>
+    ),
+  },
+  {
+    title: "E-Plant Shopping",
+    imgSrc: "/project-imgs/e-plant_shopping.png",
+    code: "https://github.com/Daniel88dev/e-plantShopping",
+    projectLink: "https://github.com/Daniel88dev/e-plantShopping",
+    tech: ["React", "Javascript", "HTML", "CSS", "Redux"],
+    description:
+      "Forked Project from IBM Fullstack course, to make React App using Redux",
+    modalContent: (
+      <>
+        <p>
+          This was Project, which was part of IBM Fullstack course, to make
+          Frontend App with usage of React, CSS and Redux.
+        </p>
+        <p>
+          Application is not deployed, it anyone can run it locally, if want to
+          see finished project.
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "express book review",
+    imgSrc: "/project-imgs/express-book-review.png",
+    code: "https://github.com/Daniel88dev/expressBookReviews",
+    projectLink: "https://github.com/Daniel88dev/expressBookReviews",
+    tech: ["Node.js", "Express", "Axios"],
+    description:
+      "Project for IBM Fullstack course to manage Book Reviews, and also to display reviews of other users",
+    modalContent: (
+      <>
+        <p>
+          Project for IBM Fullstack course to manage Book Reviews, and also to
+          display reviews of other users
+        </p>
+        <p>
+          Target of course was to show skills using Node.js Express backend API,
+          with simple authentication of user using json web token on every
+          request.
+        </p>
+        <p>
+          Its just simple CRUD API service with no HTML front end. Can be tested
+          using Postman, CRUD or by any other API testing tool.
+        </p>
+      </>
+    ),
+  },
+  {
     title: "DTC List",
     imgSrc: "/project-imgs/next-dtc-list.png",
     code: "https://github.com/Daniel88dev/next-dtc-list",
-    projectLink: "https://next-dtc-list.vercel.app\n",
+    projectLink: "https://next-dtc-list.vercel.app",
     tech: [
       "React",
       "Typescript",
